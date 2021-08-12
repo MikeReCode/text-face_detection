a812ee06a8c74079815f7830435c70f188a5e70f
MikeReCode <mihail.radu.work@gmail.com>
2021-08-12 21:17:42 +0300

    Update in README file and main code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# text-face_detection
+# Text & Face Detection
+
+### Description:
+
+This program takes a zip folder with images from newspapers (other images that contain text and human faces).
+The user enters a word he wants to search for in the text present in the image and if the word is found in the text, the program will detect all the human faces in the image and will create a new contact-sheet with all the faces.
+
+### Necessary libraries:
+
+- Pillow
+- pytesseract
+- opencv-python
+- numpy
+
+### Exemple:
+
+If we were to use the image `a-0.png` in `images.zip` file and search for "Mark" wee should see the following image:
+
+![](need/a-0_output.png)
+
+Sometimes the word we are looking for is in the text but no face is found.
--- face_text_detection.py
+++ face_text_detection.py
@@ -1,5 +1,5 @@
 """
-*** Coursera Python 3 Programming Specialization - final project  ***
+*** Coursera Python 3 Programming Specialization - final project - Text & Face detection ***
 """
 
 import zipfile
@@ -16,10 +16,13 @@ import numpy as np
 # loading the face detection classifier
 face_cascade = cv.CascadeClassifier('need/haarcascade_frontalface_default.xml')
 
-# the rest is up to you!
-serch_for = input("Serch for specific word in paper ")
+
+search_for = input("Search for specific word in newspaper ")
+# If the word is found in the text, the program will search for all human faces
+# and will retrieve a contact-sheet with all faces
 
 
+# Open zip file with all images
 with zipfile.ZipFile('need/images.zip', 'r') as zfile:
     for file in zfile.namelist():
         data = zfile.open(file)
@@ -31,23 +34,23 @@ with zipfile.ZipFile('need/images.zip', 'r') as zfile:
         # pillow image
         pil_img = Image.fromarray(pic_arr)
 
-        # Convert to grey collor
+        # Convert to grey color
         gray = cv.cvtColor(pic_arr, cv.COLOR_BGR2GRAY)
 
-        # serch text in image using tesseract
+        # search text in image using tesseract
         text = pytesseract.image_to_string(gray)
-        # text = "and"
-        if serch_for in text:
+
+        if search_for in text:
             print("Results found in file {} ".format(file))
 
-            # serch human faces in image using openCV
+            # search human faces in image using openCV
             faces = face_cascade.detectMultiScale(gray, 1.30)
 
             if len(faces) == 0:
-                print("But ther were no faces in file!")
+                print("But there were no faces in file!")
                 continue
 
-            # size of every image in the contac sheet
+            # size of every image in the contact sheet
             size = (110, 110)
 
             img_faces = []
@@ -55,16 +58,16 @@ with zipfile.ZipFile('need/images.zip', 'r') as zfile:
             for rec in faces.tolist():
                 # Setup drawing context
                 # drawing=ImageDraw.Draw(pil_img)
-                # And draw the new box
+                # And draw the new box around every face
                 # drawing.rectangle((rec[0],rec[1],rec[0]+rec[2],rec[1]+rec[3]), outline="black")
 
                 # crop the face image
-                recorte = pil_img.crop((rec[0], rec[1], rec[0] + rec[2], rec[1] + rec[3]))
+                face_crop = pil_img.crop((rec[0], rec[1], rec[0] + rec[2], rec[1] + rec[3]))
 
                 # resize the image
-                recorte.thumbnail(size)
+                face_crop.thumbnail(size)
 
-                img_faces.append(recorte)
+                img_faces.append(face_crop)
 
             first_image = img_faces[0]
 
@@ -82,5 +85,4 @@ with zipfile.ZipFile('need/images.zip', 'r') as zfile:
                 else:
                     x = x + size[0]
 
-            # display(contact_sheet)
             contact_sheet.show()
